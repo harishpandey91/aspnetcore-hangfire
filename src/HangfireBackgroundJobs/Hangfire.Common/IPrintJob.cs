0244895bd1e5a00bc810fcc3eae8227bf76b576c
Harish Pandey <harishpandey.it@live.com>
2021-06-12 22:35:03 +0530

    initial commit

---
+++ src/HangfireBackgroundJobs/Hangfire.Common/IPrintJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ReSharper disable once IdentifierTypo
+namespace Hangfire.Jobs
+{
+    public interface IPrintJob
+    {
+        void Print();
+    }
+}
---
+++ src/HangfireBackgroundJobs/Hangfire.Common/PrintJob.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// ReSharper disable once IdentifierTypo
+namespace Hangfire.Jobs
+{
+    public class PrintJob : IPrintJob
+    {
+        public void Print()
+        {
+            Console.WriteLine($"Hang-fire recurring print job!");
+        }
+    }
+}
